123c927bff945ba7d052a3e54d081d52d7fef0ce
5Gdevs <5gdeveloper2024@gmail.com>
2025-09-27 09:26:30 -0700

    allow multiple domains from .env

--- src/server.ts
+++ src/server.ts
@@ -14,12 +14,22 @@ const app: Express = express()
 // Set the application to trust the reverse proxy
 app.set('trust proxy', true)
 
+const allowedOrigins = env.CORS_ORIGIN.split(',');
 
 // Middlewares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/static', express.static('public'))
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }))
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    },
+    credentials: true,
+}));
 app.use(helmet())
 // app.use(rateLimiter)
 
